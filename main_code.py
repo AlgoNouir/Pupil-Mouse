6faef1efeaddc116885e0666d4f55bc5707c5c48
algonouir <algo.mahdi.nouri@gmail.com>
2024-04-08 12:21:12 +0330

    create better

--- main_code.py
+++ main_code.py
@@ -1,52 +1,160 @@
+import re
 import numpy as np
 import cv2
+from skimage import restoration
+from skimage.feature import blob_doh
+
+
+
+def nothing(x):
+    pass
+
+def rgb(*data):
+    return np.array(data)
+
+cv2.namedWindow('image')
+
+
+cv2.createTrackbar('param1','image',0,100,nothing)
+cv2.createTrackbar('param2','image',0,100,nothing)
+cv2.createTrackbar('param3','image',0,100,nothing)
+
+cv2.setTrackbarPos('param1', 'image', 5)
+cv2.setTrackbarPos('param2', 'image', 2)
+cv2.setTrackbarPos('param3', 'image', 2)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
+
+
 #number signifies camera
 cap = cv2.VideoCapture(2)
 
+
+
+def detect_eyes(img):
+    eyes = eye_cascade.detectMultiScale(img, 1.3, 5) # detect eyes
+    width = np.size(img, 1) # get face frame width
+    height = np.size(img, 0) # get face frame height
+    left_eye = None
+    right_eye = None
+
+    for (x, y, w, h) in eyes:
+        if y > height / 2:
+            pass
+        eyecenter = x + w / 2  # get the eye center
+        if eyecenter < width * 0.5:
+            left_eye = ((x, y, w, h), img[y:y + h, x:x + w])
+        else:
+            right_eye = ((x, y, w, h), img[y:y + h, x:x + w])
+    return left_eye, right_eye
+
+def detect_faces(img):
+    coords = face_cascade.detectMultiScale(img, 1.3, 5)
+    frame = None
+    i = None
+    if len(coords) > 1:
+        biggest = (0, 0, 0, 0)
+        for i in coords:
+            if i[3] > biggest[3]:
+                biggest = i
+        biggest = np.array([i], np.int32)
+    elif len(coords) == 1:
+        biggest = coords
+    else:
+        return None
+    for (x, y, w, h) in biggest:
+        frame = ((x, y, w, h), img[y:y + h, x:x + w])
+        
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+    return frame
+
+def cut_eyebrows(img):
+    height, width = img.shape[:2]
+    eyebrow_h = int(height / 4)
+    img = img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)
+    return img
+
+
+
+def getPupilCenterData(img):
+    
+    param1 = cv2.getTrackbarPos('param1','image')
+    param2 = cv2.getTrackbarPos('param2','image')
+    param3 = cv2.getTrackbarPos('param3','image')
+
+    
+    faceData = detect_faces(img)
+    
+    if faceData is None:
+        return None
+    
+    (fx, fy, fw, fh), face = faceData
+    
+    eyes = detect_eyes(face)
+    
+    result = []
+    for eyeData in eyes:
+        
+        if eyeData is None:
+            result.append(None)
+            continue
+        
+        (ex, ey, ew, eh), eye = eyeData
+        ex += fx
+        ey += fy
+        _, eye = cv2.threshold(eye, 0, 255, cv2.THRESH_BINARY)
+
+        Y, X = np.where(eye==255)
+        Z = np.column_stack((X,Y)).astype(np.float32)
+
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, param1, param2)
+        nClusters = 1
+        ret,label,center=cv2.kmeans(Z,nClusters,None,criteria,param3,cv2.KMEANS_RANDOM_CENTERS)
+        
+        
+        cX, cY = center[0]
+        cX += ex
+        cY += ey
+        
+        result.append((int(cX), int(cY)))
+        
+    return result        
+
+left_means = []
+right_means = []
+
 while 1:
     ret, img = cap.read()
+    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # Apply Gaussian blur to reduce noise and smoothen edges 
-    blurred = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=0.5) 
-      
-    # Perform Canny edge detection 
-    edges = cv2.Canny(blurred, 70, 135) 
     
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    eyes = eye_cascade.detectMultiScale(gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-        roi_gray2 = gray[ey:ey+eh, ex:ex+ew]
-        roi = img[ey:ey+eh, ex:ex+ew]
-        circles = cv2.HoughCircles(roi_gray2,cv2.HOUGH_GRADIENT,1,200,param1=200,param2=1)
+    if len(left_means) > 100:
+        del left_means[-1]
+    if len(right_means) > 100:
+        del right_means[-1]
+    
+    centers = getPupilCenterData(gray)
+    
+    if centers is None:
+        continue
+    
+    left, right = centers
+    
+    if left is not None:
+        left_means.append(left)
+    
+    xMean, yMean = np.array(left_means).mean(axis=0)
+    cv2.circle(img, (xMean, yMean), 5, (0,255,0), -1)
 
-        rows, cols, _ = roi.shape
-        gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray_roi = cv2.GaussianBlur(gray_roi, (7, 7), 0)
-        _, threshold = cv2.threshold(gray_roi, 3, 255, cv2.THRESH_BINARY_INV)
-        contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
-        for cnt in contours:
-            (x, y, w, h) = cv2.boundingRect(cnt)
-            #cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
-            cv2.rectangle(roi, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.line(roi, (x + int(w/2), 0), (x + int(w/2), rows), (0, 255, 0), 2)
-            cv2.line(roi, (0, y + int(h/2)), (cols, y + int(h/2)), (0, 255, 0), 2)
-            break
-        
-        cv2.imshow("Threshold", threshold)
-        cv2.imshow("gray roi", gray_roi)
-        cv2.imshow("Roi", roi)
-    cv2.imshow('img',img)
-    cv2.imshow('edges',edges)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+    
+    cv2.imshow('image',img)
+
 
 cap.release()
 cv2.destroyAllWindows()
